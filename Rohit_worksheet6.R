ae66d5452703c48d547f47b7fcb51453f6a0cbdf
rohitdutta22 <110178330+rohitdutta22@users.noreply.github.com>
2022-08-24 14:47:12 +0530

    Update Rohit_worksheet6.R

--- Rohit_worksheet6.R
+++ Rohit_worksheet6.R
@@ -1,6 +1,6 @@
 rm(list = ls())
-library("tidyverse")
 library(rvest)
+library(stringr)
 html <- read_html("https://www.iitk.ac.in/math/faculty")
 
 name <- html %>% html_elements(".head3 a") %>% html_text()
@@ -13,15 +13,33 @@ name1
 
 
 html2 <- read_html("https://www.imdb.com/chart/top/")
-name2 <- html2 %>% html_elements(".titleColumn a") %>% html_text()
-name2
+
+movie.name <- html2 %>% html_elements(".titleColumn a") %>% html_text()
+movie.name
 
 
-x<- "apple is red"
-substring(x,10,12)
+movie.year <- html2 %>% html_elements(".secondaryInfo") %>% html_text()
+movie.year <- as.numeric(str_sub(movie.year,2,-2))
+movie.year
+
+
+movie.rating <- html2 %>% html_elements(".ratingColumn strong") %>% html_text()
+movie.rating <- as.numeric(movie.rating)
+movie.rating
+
+
+movie.votes <- html2 %>% html_elements("strong") %>% html_attr("title")
+movies.votes <- as.numeric(gsub(",","",str_sub(movie.votes,13,-13)))
+movies.votes
+
+data.frame(movie.name,movie.year,movie.rating,movies.votes)
 
 
 
 
 
-name3 <- html2 %>% html_elements(".ratingColumn imdbRating strong") %>% html_attr(strong,substring(title,14,18)) ;name3
+nam <- html2 %>% html_elements(".posterColumn span") %>% html_attr("name")
+ind = which(nam == "nv")
+nam1 <- html2 %>% html_elements(".posterColumn span") %>% html_attr("data-value")
+as.numeric(nam1[ind])
+

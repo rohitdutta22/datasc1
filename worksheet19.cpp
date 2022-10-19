60e74dd16097165922625409763a89fa21795040
rohitdutta22 <110178330+rohitdutta22@users.noreply.github.com>
2022-10-19 17:04:55 +0530

    Update worksheet19.cpp

--- worksheet19.cpp
+++ worksheet19.cpp
@@ -33,7 +33,7 @@ NumericVector funcC(NumericVector x){
 
 // [[Rcpp::export]]
 NumericMatrix addmat(NumericMatrix x, NumericMatrix y){
-
+  
   double m  = x.nrow();
   double n = x.ncol();
   NumericMatrix sum_mat(m , n);
@@ -62,22 +62,63 @@ NumericVector column_sums(NumericMatrix x){
   NumericVector col_sums(n);
   for(int j = 0; j < n; j++){
     
-     double sum = 0;
+    double sum = 0;
     
-     for(int i = 0; i < m; i++){
-       
-       sum = sum + x(i,j);
-     }
+    for(int i = 0; i < m; i++){
+      
+      sum = sum + x(i,j);
+    }
     col_sums[j] = sum;
   }
   return col_sums;
 }
 
+// [[Rcpp::export]]
+LogicalVector check_logical(NumericVector x){
+  int n = x.size();
+  LogicalVector logic(n);
+  for(int i = 0; i < n; i++){
+    if(x[i] > 0){
+      logic[i] = TRUE;
+    }else{
+      logic[i] = FALSE;
+    }
+  }
+  return logic;
+}
 
 
+// [[Rcpp::export]]
+NumericMatrix prb4(int n, double rho){
+  
+  NumericMatrix req_mat(n, n);
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      req_mat(i,j) = pow(rho, abs(i - j));
+    }
+  }
+  return req_mat;
+}
 
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
+
+// [[Rcpp::export]]
+double attempts(){
+  
+  int sum1 = 0;
+  NumericVector attemp_vec(1000);
+  for(int i = 0; i < 1000; i++){
+    double sum = 0;
+    int count = 0;
+  while(sum <= 1)
+  {
+    sum = sum + R::runif( 0, 1);
+    count = count + 1;
+  }
+  attemp_vec[i] = count;
+  sum1 = sum1 + count;
+  }
+  return sum1/1000.0;
+}
+
+
 
